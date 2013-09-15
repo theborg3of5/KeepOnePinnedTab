d8f8ee525cba6be9099f237c4a10bc650133e318
theborg3of5 <theborg3of5@gmail.com>
2013-09-14 21:23:39 -0500

    Bugfixing for additional windows, and allowing selection of special tab.

--- background.js
+++ background.js
@@ -1,21 +1,39 @@
 ﻿var creatingTab = false;
+// var goToSecond = true;
 
 function keepSpecialPinned() {
 	if(!creatingTab) {
-		chrome.windows.getCurrent({populate: true}, function(window){
-			// Make sure we've got our special tab in place.
-			var firstTab = window.tabs[0];
-			if(!firstTab.pinned || firstTab.url !== "chrome://newtab/") {
-				// Make our special tab. Stick it at the beginning, pin it, and don't focus it.
-				creatingTab = true;
-				chrome.tabs.create({"index": 0, "pinned": true, "active": false}, function(tab) {
-					creatingTab = false;
-				});
+		chrome.windows.getAll({populate: true}, function(windows){
+			// Snag the current window from the masses.
+			var window;
+			for(var i = 0; i < windows.length; i++) {
+				if(windows[i].focused) {
+					window = windows[i];
+					break;
+				}
 			}
 			
-			// Ensure that there's at least one tab after our special one.
-			if(window.tabs.length === 1) {
-				chrome.tabs.create({"active": true});
+			// Don't check firstt ab of apps and popups.
+			if(window.type === "normal") {
+				// Make sure we've got our special tab in place.
+				var firstTab = window.tabs[0];
+				if(!firstTab.pinned || firstTab.url !== "chrome://newtab/") {
+				
+					// Spawn our special tab. Stick it at the beginning, pin it, and don't focus it.
+					creatingTab = true;
+					chrome.tabs.create({"index": 0, "pinned": true, "active": false}, function(tab) {
+						creatingTab = false;
+					});
+					
+				}
+				
+				// Ensure that there's at least one tab after our special one.
+				if(getTotalNumberOfTabs(windows) === 1) {
+					creatingTab = true;
+					chrome.tabs.create({"active": true}, function(tab){
+						creatingTab = false;
+					});
+				}
 			}
 			
 			// Close any new-page-tabs that are not the special one or the current one.
@@ -30,18 +48,38 @@ function keepSpecialPinned() {
 	}
 }
 
-// If our special tab was the one activated, then switch away, not for you!
-function tabActivated(activeInfo) {
-	chrome.windows.getCurrent({populate: true}, function(window){
-		if(!creatingTab && window.tabs[0].id === activeInfo.tabId && window.tabs[0].pinned && window.tabs[0].url === "chrome://newtab/") {
-			chrome.tabs.update(window.tabs[1].id, {active: true});
-		}
-	});
+// // If our special tab was the one activated, then switch away, not for you!
+// function tabActivated(activeInfo) {
+	// chrome.windows.getCurrent({populate: true}, function(window){
+		// if(!creatingTab && window.tabs[0].id === activeInfo.tabId && window.tabs[0].pinned && window.tabs[0].url === "chrome://newtab/") {
+		
+			// // Alternate going to the second or last tabs when this one is activated.
+			// if(goToSecond) {
+				// chrome.tabs.update(window.tabs[1].id, {active: true});
+				// goToSecond = false;
+			// } else {
+				// chrome.tabs.update(window.tabs[window.tabs.length - 1].id, {active: true});
+				// goToSecond = true;
+			// }
+			
+		// }
+	// });
+// }
+
+function getTotalNumberOfTabs(windows) {
+	var tab_count;
+	for (var i = 0;  i < windows.length; i++) {
+		var w = windows[i];
+		tab_count += w.tabs.length;
+	}
+	
+	return tab_count;
 }
 
 // Boot it up and schedule functions for events.
 keepSpecialPinned();
 chrome.tabs.onCreated.addListener(keepSpecialPinned);
 chrome.tabs.onRemoved.addListener(keepSpecialPinned);
-chrome.tabs.onActivated.addListener(tabActivated);
+chrome.tabs.onUpdated.addListener(tabUpdated);
+// chrome.tabs.onActivated.addListener(tabActivated);
 
