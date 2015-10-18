9decc0e0541ff0a9043de433f98f088ed8ebf630
Gavin Borg <theborg3of5@gmail.com>
2015-10-18 17:26:35 -0500

    Adding options to extension now; rest of update in previous commit.

---
+++ options.js
@@ -0,0 +1,26 @@
+﻿// Saves options to chrome sync storage.
+function saveOps() {
+	chrome.storage.sync.set({
+		"KeepOnePinnedTab_NewTabPage": document.getElementById("newTabPage").value
+	});
+
+	// Update status to let user know options were saved.
+	var status = document.getElementById("status");
+	status.innerHTML = "Options Saved.";
+	setTimeout(function() {
+		status.innerHTML = "";
+	}, 750);
+}
+
+// Restores select box state to saved value from chrome sync storage.
+function loadOps() {
+	chrome.storage.sync.get(["KeepOnePinnedTab_NewTabPage"], 
+		function(items) {
+			document.getElementById("newTabPage").value = items["KeepOnePinnedTab_NewTabPage"];
+		}
+	);
+}
+
+// Add the events to load/save from this page.
+document.addEventListener('DOMContentLoaded', loadOps);
+document.querySelector('#save').addEventListener('click', saveOps);
