f3491fc4dee22081a470a010e1b90d5b9151e815
Gavin Borg <theborg3of5@gmail.com>
2017-01-15 18:40:54 -0600

    Major rework of storage, add radio buttons for which tab, cleanup.

--- options.js
+++ options.js
@@ -1,9 +1,22 @@
-﻿// Saves options to chrome sync storage.
-function saveOps() {
-	chrome.storage.sync.set({
-		"KeepOnePinnedTab_NewTabPage": document.getElementById("newTabPage").value
-	});
+﻿var PinnedTabPage_Default    = "Default";
+var PinnedTabPage_BlankLight = "BlankLight";
+var PinnedTabPage_BlankDark  = "BlankDark";
+var PinnedTabPage_Custom     = "Custom";
 
+// Saves options to chrome sync storage.
+function saveOptions() {
+	var customURL;
+	
+	var pinnedTabPage = document.querySelector("input[name=PinnedTabPage]:checked").value;
+	if(pinnedTabPage == PinnedTabPage_Custom)
+		customURL = document.querySelector("#customURL").value;
+	
+	chrome.storage.sync.set({
+		"KeepOnePinnedTab_PinnedTabPage": pinnedTabPage,
+		"KeepOnePinnedTab_CustomPinnedTabURL": customURL,
+		"KeepOnePinnedTab_NewTabPage": "" // Old style, clear it out when we save. Should have fixed it to new style in actual elements via loadOptions().
+	});
+	
 	// Update status to let user know options were saved.
 	var status = document.getElementById("status");
 	status.innerHTML = "Options Saved.";
@@ -13,14 +26,40 @@ function saveOps() {
 }
 
 // Restores select box state to saved value from chrome sync storage.
-function loadOps() {
-	chrome.storage.sync.get(["KeepOnePinnedTab_NewTabPage"], 
+function loadOptions() {
+	chrome.storage.sync.get(
+		[
+			"KeepOnePinnedTab_PinnedTabPage",
+			"KeepOnePinnedTab_CustomPinnedTabURL",
+			"KeepOnePinnedTab_NewTabPage" // Old
+		],
 		function(items) {
-			document.getElementById("newTabPage").value = items["KeepOnePinnedTab_NewTabPage"];
+			var pinnedTabPage = items["KeepOnePinnedTab_PinnedTabPage"];
+			var customURL     = items["KeepOnePinnedTab_CustomPinnedTabURL"];
+			
+			// Old style of storage (remove eventually)
+			var oldStyleURL = items["KeepOnePinnedTab_NewTabPage"];
+			if((oldStyleURL != undefined) && (oldStyleURL != "")) {
+				pinnedTabPage = PinnedTabPage_Custom;
+				customURL = oldStyleURL;
+			}
+			
+			// Default
+			if( (pinnedTabPage == undefined) || (pinnedTabPage == "") )
+				pinnedTabPage = PinnedTabPage_Default;
+			
+			var optionElement = document.querySelector("input[name=PinnedTabPage][value=" + pinnedTabPage + "]");
+			if(optionElement)
+				optionElement.checked = true;
+			else
+				alert("curses " + pinnedTabPage);
+			
+			if(pinnedTabPage == PinnedTabPage_Custom)
+				document.getElementById("customURL").value = customURL;
 		}
 	);
 }
 
 // Add the events to load/save from this page.
-document.addEventListener('DOMContentLoaded', loadOps);
-document.querySelector('#save').addEventListener('click', saveOps);
+document.addEventListener("DOMContentLoaded", loadOptions);
+document.querySelector("#save").addEventListener("click", saveOptions);
